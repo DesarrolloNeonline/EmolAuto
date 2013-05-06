70a3d20a13a382e9076e1070ad57f2b7c78a5516
root <root@club.mersap.com>
2013-05-06 19:04:39 +0000

    Commit MAY06MAC1502

---
+++ modal-enviar.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Emol autom&oacute;viles</title>
+  <meta name="description" content="Autos nuevos, Autos usados, jeep, camionetas, todo terreno, compra y venta, Automviles EMOL">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="css/normalize.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <script type="text/javascript" src="js/vendor/jquery-1.7.1.js"></script>
+  <script type="text/javascript" src="js/vendor/modernizr-2.6.2.min.js"></script>
+  
+  <script type="text/javascript">
+	function showhide(divid, state){
+	  document.getElementById(divid).style.display=state
+	}
+  </script>
+  
+</head>
+
+<body>
+  
+  <div id="content_modal_enviar">
+  
+    <div id="top_modal">
+      <h1>Enviar avisos seleccionados</h1>
+      <div class="content_Item_form_modal">
+        <label>Correo</label>
+        <input name="Year" type="text" value="" placeholder="Ej: correo@correo.cl"/>
+        <input name="Enviar" type="button" value="Enviar" />
+      </div>
+    </div>
+    
+    <div id="content_List_selected">
+    
+      <ul class="List_Autos_selected">
+        
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        <li>
+          <div class="img_Auto_list"><img src="images/auto.jpg" alt="Auto" /></div>
+          <div class="content_Txt_list">
+            <strong>Mercedes Benz Motor Home</strong><br />
+            <span>$ 18.000.000</span><br />
+            2006
+          </div>
+        </li>
+        
+      </ul>
+    
+    </div>
+  
+
+  </div>
+
+</body>
+</html>
